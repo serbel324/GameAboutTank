b9c2593902647331ab2c9fd7b4b79e1e1b0a0b02
Danila Gulyaev <mrsousgulash@gmail.com>
2024-05-25 14:01:47 +0300

    Turret rotation

--- Tank/Turret.gd
+++ Tank/Turret.gd
@@ -8,6 +8,7 @@ enum State {
 @export var rotation_speed : float = 1
 
 var angular_speed : float = 0
+var angle_to_target : float = 0
 
 var state : State
 var target : Vector2
@@ -20,8 +21,9 @@ func process_input():
 	var position_on_canvas : Vector2 = get_global_transform_with_canvas().get_origin()
 	var forward : Vector2 = global_transform.basis_xform(Vector2.RIGHT)
 	var direction_to_target : Vector2 = target - position_on_canvas
+	angle_to_target = forward.angle_to(direction_to_target)
 	
-	if (forward.cross(direction_to_target) < 0):
+	if (angle_to_target > 0):
 		angular_speed = rotation_speed
 	else:
 		angular_speed = -rotation_speed
@@ -32,7 +34,13 @@ func _ready():
 
 func state_alive(delta : float):
 	process_input()
-	rotate(angular_speed * delta)
+	
+	var delta_angle : float = angular_speed * delta
+	if (delta_angle > 0 && angle_to_target > 0):
+		delta_angle = minf(delta_angle, angle_to_target)
+	elif (delta_angle < 0 && angle_to_target < 0):
+		delta_angle = maxf(delta_angle, angle_to_target)
+	rotate(delta_angle)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
