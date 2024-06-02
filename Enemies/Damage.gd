3ecd9367a2b4507a862f684ff4ee59630573d911
Sergey Belyakov <serbel324@gmail.com>
2024-06-02 03:41:13 +0300

    Basic entities

---
+++ Enemies/Damage.gd
@@ -0,0 +1,6 @@
+class_name Damage
+
+@export var damage_pts : float = 0
+
+func _init(damage : float):
+	damage_pts = damage
---
+++ Enemies/Zombie.gd
@@ -0,0 +1,36 @@
+extends Entity
+
+@export var speed = 1
+
+var target : Node2D = null
+var linear_movement : Vector2 = Vector2(0, 0)
+var direction : Vector2 = Vector2.RIGHT
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	target = get_parent().find_child("Tank") as Node2D
+	print(target)
+
+
+func state_alive(_delta : float):
+	if (target != null):
+		direction = (target.global_position - global_position).normalized()
+		linear_movement = direction * speed
+	else:
+		direction = Vector2.RIGHT
+		linear_movement = Vector2(0, 0)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta : float):
+	match state:
+		State.ALIVE:
+			state_alive(delta)
+		State.DEAD:
+			queue_free()
+
+
+func _integrate_forces(physics_state : PhysicsDirectBodyState2D):
+	physics_state.linear_velocity = linear_movement
+	rotation = Vector2.RIGHT.angle_to(direction)
