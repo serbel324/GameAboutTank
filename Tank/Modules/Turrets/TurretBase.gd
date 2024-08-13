4a45ea669701a08f46e4c31b0f94cced301f92f6
D3c3453d <sergey.et02@gmail.com>
2024-08-13 22:45:06 +0300

    fix: now update_camera_position takes camera.zoom into account

--- Tank/Modules/Turrets/TurretBase.gd
+++ Tank/Modules/Turrets/TurretBase.gd
@@ -6,27 +6,26 @@ enum State {
 	DEAD
 }
 
-@export var camera_view_distance : float = 0.3
-@export var rotation_speed : float = 1.0
+@export var camera_view_distance: float = 0.8
+@export var rotation_speed: float = 1.0
 
-var state : State = State.ALIVE
-var angular_speed : float = 0
+var state: State = State.ALIVE
+var angular_speed: float = 0
 
-var target : Vector2
-var tank : TankController = null
-var camera : Node2D = null
+var target: Vector2
+var tank: TankController = null
+var camera: Camera2D = null
 
 
 func die() -> void:
 	state = State.DEAD
 
 
-func set_target(target_global : Vector2) -> void:
+func set_target(target_global: Vector2) -> void:
 	target = target_global
 
-
-func update_camera_position(shift : Vector2):
-	camera.position = shift * camera_view_distance + global_position
+func update_camera_position(shift: Vector2):
+	camera.position = shift * camera_view_distance / camera.zoom + global_position
 
 
 # Called when the node enters the scene tree for the first time.
@@ -35,21 +34,21 @@ func _ready():
 	tank = get_node("/root/World/Tank") as TankController
 	assert(tank != null)
 	
-	camera = get_node("/root/World/PlayerCamera")
+	camera = get_node("/root/World/PlayerCamera") as Camera2D
 	assert(camera != null)
 
 
-func rotate_to_target(delta : float) -> void:
-	var forward : Vector2 = global_transform.basis_xform(Vector2.RIGHT)
-	var direction_to_target : Vector2 = target - global_position
-	var angle_to_target : float = forward.angle_to(direction_to_target)
+func rotate_to_target(delta: float) -> void:
+	var forward: Vector2 = global_transform.basis_xform(Vector2.RIGHT)
+	var direction_to_target: Vector2 = target - global_position
+	var angle_to_target: float = forward.angle_to(direction_to_target)
 	
 	if (angle_to_target > 0):
 		angular_speed = rotation_speed
 	else:
 		angular_speed = -rotation_speed
 	
-	var delta_angle : float = angular_speed * delta
+	var delta_angle: float = angular_speed * delta
 	if (delta_angle > 0 && angle_to_target > 0):
 		delta_angle = minf(delta_angle, angle_to_target)
 	elif (delta_angle < 0 && angle_to_target < 0):
@@ -57,7 +56,7 @@ func rotate_to_target(delta : float) -> void:
 	rotate(delta_angle)
 	
 
-func state_alive(delta : float) -> void:
+func state_alive(delta: float) -> void:
 	rotate_to_target(delta)
 
 
